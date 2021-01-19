801f35a5e47da1df6f54611b652257936e99a0fc
Faith <oyebodefaith3@gmail.com>
2021-01-20 00:17:46 +0100

    setup redux, crated actions and reducers for homepage and product page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,35 +1,40 @@
- import React, { useState, useEffect } from 'react';
+ import React, { useEffect } from 'react';
  import { Row, Col } from 'react-bootstrap';
- import axios from 'axios';
+ import { useDispatch, useSelector } from 'react-redux';
  import Product from '../components/Product';
-
+ import Message from '../components/Message';
+ import Loader from '../components/Loader';
+ import { listProducts } from '../actions/productActions';
+ 
  
  const HomePage = () => {
-
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
+        dispatch(listProducts()); 
 
-            setProducts(data);
-        }
+    }, [dispatch]);
 
-        fetchProducts();
-    }, []);
 
      return (
          <>
             <h1>Latest Products</h1>
-            <Row>
-             {
-                products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                        <Product product={product} />
-                    </Col>
-                ))
-             }
-            </Row>
+            { loading ? 
+                (<Loader />) : 
+                error ? 
+                (<Message variant='danger'>{error}</Message>) : 
+                (<Row>
+                    {
+                    products.map(product => (
+                        <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                            <Product product={product} />
+                        </Col>
+                    ))
+                    }
+                </Row>)
+            }
          </>
      )
  }
