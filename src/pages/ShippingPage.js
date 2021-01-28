e3611812fa0499733c0864df7374cd854abcf6a4
Faith <oyebodefaith3@gmail.com>
2021-01-28 09:21:53 +0100

    created checkout stps component

--- src/pages/ShippingPage.js
+++ src/pages/ShippingPage.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
 import { saveShippingAddress } from '../actions/cartActions';
 
 const ShippingPage = () => {
@@ -28,6 +29,7 @@ const ShippingPage = () => {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
