bc8f43406b43e88ce4f4f8f0362bf1d3ed365a57
Faith <oyebodefaith3@gmail.com>
2021-02-08 19:31:34 +0100

    resolved anticipated security issues on admin user list page

--- src/pages/UserListPage.js
+++ src/pages/UserListPage.js
@@ -12,16 +12,25 @@ import { listUsers } from '../actions/userActions';
 const UserListPage = () => {
     const dispatch = useDispatch();
 
+    const history = useHistory();
+    
     const userList = useSelector(state => state.userList);
     const { loading, error, users } = userList;
+
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
 
     const deleteHandler = (id) => {
         console.log('delete');
     }
 
     useEffect(() => {
-        dispatch(listUsers());
-    }, [dispatch]);
+        if(userInfo && userInfo.isAdmin){
+            dispatch(listUsers());
+        }else{
+            history.push('/login')
+        }
+    }, [dispatch, history, userInfo]);
 
     return (
         <>
