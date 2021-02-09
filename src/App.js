55fb20219f373399efeec97cec01756156a88dd7
Faith <oyebodefaith3@gmail.com>
2021-02-09 15:45:34 +0100

    added the functionality for admin to update users details by clicking on the edit button on the UserList page

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ import PaymentPage from './pages/PaymentPage';
 import PlaceOrderPage from './pages/PlaceOrderPage';
 import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
-
-
-
-
+import UserEditPage from './pages/UserEditPage';
 
 
 
@@ -37,6 +34,7 @@ const App = () => {
             <Route path='/placeorder' component={PlaceOrderPage} exact />
             <Route path='/order/:id' component={OrderPage} exact />
             <Route path='/admin/userlist' component={UserListPage} exact />
+            <Route path='/admin/user/:id/edit' component={UserEditPage} />
 
         </Container>
       </main>
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -20,7 +20,10 @@ import {
     USER_LIST_RESET,
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
-    USER_DELETE_FAIL,  
+    USER_DELETE_FAIL,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,  
 } from "../constants/userConstants";
 import { MYORDER_LIST_RESET } from "../constants/orderConstants";
 
@@ -209,7 +212,7 @@ export const deleteUser = (id) => async (dispatch, getState) => {
             }
         }
 
-        const { data } = await axios.delete(`/api/users/${id}`, config);
+        await axios.delete(`/api/users/${id}`, config);
 
         dispatch({ type: USER_DELETE_SUCCESS });
         
@@ -220,4 +223,36 @@ export const deleteUser = (id) => async (dispatch, getState) => {
         });
     }
 }
+
+
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST
+        });
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.put(`/api/users/${user._id}`, user, config);
+
+        dispatch({ type: USER_UPDATE_SUCCESS });
+
+        dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+
+        
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
+            payload: error?.response?.data?.message ?? error.message
+        });
+    }
+}
+
 
