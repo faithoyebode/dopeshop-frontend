f7da7493a6838e6844215762499c4c80dc67bf2e
Faith <oyebodefaith3@gmail.com>
2021-02-10 07:45:58 +0100

    added delete product functionality to admin's product list page

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -6,7 +6,7 @@ import { FaCheck, FaTimes, FaEdit, FaTrash, FaPlus } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts } from '../actions/productActions';
+import { listProducts, deleteProduct } from '../actions/productActions';
 import { decodeEntity } from '../utils';
 
 const ProductListPage = ({ match }) => {
@@ -17,12 +17,19 @@ const ProductListPage = ({ match }) => {
     const productList = useSelector(state => state.productList);
     const { loading, error, products } = productList;
 
+    const productDelete = useSelector(state => state.productDelete);
+    const { 
+        loading: loadingDelete, 
+        error: errorDelete, 
+        success: successDelete 
+    } = productDelete;
+
     const userLogin = useSelector(state => state.userLogin);
     const { userInfo } = userLogin;
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure?')){
-            // DELETE PRODUCTS
+            dispatch(deleteProduct(id));
         }
     }
 
@@ -36,66 +43,69 @@ const ProductListPage = ({ match }) => {
         }else{
             history.push('/login')
         }
-    }, [dispatch, history, userInfo]);
+    }, [dispatch, history, userInfo, successDelete]);
 
     
     return (
         <>
-        <Row className='align-items-center'>
-            <Col>
-                <h1>Products</h1>
-            </Col>
-            <Col className='text-right'>
-                <Button className='my-3' onClick={createProductHandler}>
-                    <FaPlus /> Create Product
-                </Button>
-            </Col>
-        </Row>
-          {loading ? 
-            <Loader /> : 
-            error ? 
-            <Message variant='danger'>{error}</Message> :
-            (
-                <Table striped bordered responsive className='table-sm'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>NAME</th>
-                            <th>PRICE</th>
-                            <th>CATEGORY</th>
-                            <th>BRAND</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map(product => (
-                            <tr key={product._id}>
-                                <td>{product._id}</td>
-                                <td>{product.name}</td>
-                                <td>
-                                    {decodeEntity('&#x20A6;')}{product.price} 
-                                </td>
-                                <td>{product.category}</td>
-                                <td>{product.brand}</td>
-                                <td>
-                                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                        <Button variant='light' className='btn-sm'>
-                                            <FaEdit />
-                                        </Button>
-                                    </LinkContainer>
-                                    <Button 
-                                        variant='danger' 
-                                        className='btn-sm'
-                                        onClick={() => deleteHandler(product._id)}    
-                                    >
-                                        <FaTrash />
-                                    </Button>
-                                </td>
+            <Row className='align-items-center'>
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className='text-right'>
+                    <Button className='my-3' onClick={createProductHandler}>
+                        <FaPlus /> Create Product
+                    </Button>
+                </Col>
+            </Row>
+            {loadingDelete && <Loader />}
+            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loading ? 
+                <Loader /> : 
+                error ? 
+                <Message variant='danger'>{error}</Message> :
+                (
+                    <Table striped bordered responsive className='table-sm'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>PRICE</th>
+                                <th>CATEGORY</th>
+                                <th>BRAND</th>
+                                <th></th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            )}   
+                        </thead>
+                        <tbody>
+                            {products.map(product => (
+                                <tr key={product._id}>
+                                    <td>{product._id}</td>
+                                    <td>{product.name}</td>
+                                    <td>
+                                        {decodeEntity('&#x20A6;')}{product.price} 
+                                    </td>
+                                    <td>{product.category}</td>
+                                    <td>{product.brand}</td>
+                                    <td>
+                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                            <Button variant='light' className='btn-sm'>
+                                                <FaEdit />
+                                            </Button>
+                                        </LinkContainer>
+                                        <Button 
+                                            variant='danger' 
+                                            className='btn-sm'
+                                            onClick={() => deleteHandler(product._id)}    
+                                        >
+                                            <FaTrash />
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                )
+            }   
         </>
     )
 }
--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -4,7 +4,10 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_DELETE_SUCCESS
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -33,5 +36,18 @@ export const productDetailsReducer = (state = { product: { reviews: [], rating: 
         default:
             return state;
 
+    }
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_DELETE_REQUEST:
+            return { loading: true };
+        case PRODUCT_DELETE_SUCCESS:
+            return { loading: false, success: true };
+        case PRODUCT_DELETE_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
     }
 }
