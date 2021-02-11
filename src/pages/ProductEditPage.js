9017a07a4a7073e65ee9d44d9039f80bef70a25f
Faith <oyebodefaith3@gmail.com>
2021-02-11 16:32:27 +0100

    integrated product details update functionality to admin's product-edit page

--- src/pages/ProductEditPage.js
+++ src/pages/ProductEditPage.js
@@ -5,7 +5,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import { listProductDetails } from '../actions/productActions';
+import { 
+    listProductDetails,
+    updateProduct
+} from '../actions/productActions';
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 
 const ProductEditPage = ({ match }) => {
@@ -27,26 +31,44 @@ const ProductEditPage = ({ match }) => {
     const productDetails = useSelector(state => state.productDetails);
     const { loading, error, product } = productDetails;
 
-    // const userUpdate = useSelector(state => state.userUpdate);
-    // const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate;
+    const productUpdate = useSelector(state => state.productUpdate);
+    const { 
+        loading: loadingUpdate, 
+        error: errorUpdate, 
+        success: successUpdate 
+    } = productUpdate;
 
     useEffect(() => {
-        if(!product.name || product._id !== productId ){
-            dispatch(listProductDetails(productId));
+        if(successUpdate){
+            dispatch({ type: PRODUCT_UPDATE_RESET });
+            history.push('/admin/productlist');
         }else{
-            setName(product.name);
-            setPrice(product.email);
-            setImage(product.image);
-            setBrand(product.brand);
-            setCategory(product.category);
-            setCountInStock(product.countInStock);
-            setDescription(product.description);
+            if(!product.name || product._id !== productId ){
+                dispatch(listProductDetails(productId));
+            }else{
+                setName(product.name);
+                setPrice(product.email);
+                setImage(product.image);
+                setBrand(product.brand);
+                setCategory(product.category);
+                setCountInStock(product.countInStock);
+                setDescription(product.description);
+            }
         }
-    }, [dispatch, history, productId, product]);
+    }, [dispatch, history, productId, product, successUpdate]);
 
     const submitHandler = (e) => {
         e.preventDefault();
-        //UPDATE PRODUCT
+        dispatch(updateProduct({
+            _id: productId,
+            name, 
+            price,
+            image,
+            brand,
+            category,
+            description,
+            countInStock
+        }));
     }
 
     return (
@@ -56,6 +78,8 @@ const ProductEditPage = ({ match }) => {
             </Link>
             <FormContainer> 
                 <h1>Edit Product</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
                 {loading ? <Loader /> : 
                     error ? 
                     <Message variant='danger'>{error}</Message> : (
