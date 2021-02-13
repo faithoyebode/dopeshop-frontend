e07de34a1b8edfe2e493c424c04419947630555f
Faith <oyebodefaith3@gmail.com>
2021-02-13 07:07:27 +0100

    integrated the functionality for users to write product reviews on the product description page

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -14,7 +14,10 @@ import {
     PRODUCT_CREATE_FAIL,
     PRODUCT_UPDATE_REQUEST,
     PRODUCT_UPDATE_SUCCESS,
-    PRODUCT_UPDATE_FAIL
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -144,3 +147,35 @@ export const updateProduct = (product) => async (dispatch, getState) => {
         });
     }
 }
+
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_REQUEST
+        });
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.post(`${process.env.REACT_APP_API}/api/products/${productId}/reviews`, review, config);
+
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_SUCCESS,
+        });
+
+        
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_FAIL,
+            payload: error?.response?.data?.message ?? error.message
+        });
+    }
+}
+
